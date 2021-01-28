d7e07d5fb9581306102fbfedff84e9cdbb459843
Valentin Briand <valentin.briand@epitech.eu>
2021-01-28 20:21:53 +0100

    Improve wording

--- macos.sh
+++ macos.sh
@@ -73,4 +73,4 @@ defaults write com.apple.dock autohide -bool true
 # Hide recent apps in the Dock
 defaults write com.apple.dock show-recents -bool false
 
-echo "Done. Please restart for all changes to take effect."
+echo "Done. Please restart the computer for all changes to take effect."
