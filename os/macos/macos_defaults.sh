24ba9714421dfbde8c07a701fec4dd831db662e3
Valentin Briand <678530+vbriand@users.noreply.github.com>
2021-02-01 15:41:33 +0100

    Set new macOS defaults

--- os/macos/macos_defaults.sh
+++ os/macos/macos_defaults.sh
@@ -3,11 +3,21 @@
 # Close "System Preferences" to keep it from overriding settings we're about to change
 osascript -e 'tell application "System Preferences" to quit'
 
+# Ask for the administrator password upfront
+sudo -v
+
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
 
+# Install fonts
 cp fonts/* ~/Library/Fonts/
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
@@ -19,7 +29,7 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightC
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 
-# Fast keyboard repeat rate
+# Set fast keyboard repeat rate
 defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
 
@@ -37,9 +47,15 @@ defaults write NSGlobalDomain AppleMetricUnits -bool true
 mkdir -p ${HOME}/Pictures/Screenshots
 defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
 
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
+
+# Allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
 
 # Show all file extensions
 defaults write -g AppleShowAllExtensions -bool true
@@ -57,11 +73,24 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Enable snap-to-grid for icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
 # Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 # Show the ~/Library folder
-chflags nohidden ~/Library
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
 
 ###############################################################################
 # Dock                                                                        #
@@ -72,5 +101,32 @@ defaults write com.apple.dock autohide -bool true
 
 # Hide recent apps in the Dock
 defaults write com.apple.dock show-recents -bool false
+
+###############################################################################
+# Photos                                                                      #
+###############################################################################
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+###############################################################################
+# Shell                                                                       #
+###############################################################################
+
+# Set zsh from Homebrew as the default shell
+sudo dscl . -create /Users/${USER} UserShell $(brew --prefix)/bin/zsh
+chmod 711 $(brew --prefix)/share/zsh $(brew --prefix)/share/zsh/site-functions
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "cfprefsd" \
+  "Dock" \
+  "Finder" \
+  "Photos" \
+  "SystemUIServer"; do
+  killall "${app}" &> /dev/null
+done
 
 echo "Done. Please restart the computer for all changes to take effect."
